c01f37db02d14bb1c84dd9a4ec9b1ae42f7c99e3
Matthew Bledsoe <mbledsoe4@student.alamo.edu>
2023-03-18 14:21:56 -0500

    Initial Commit Lesson #13

---
+++ src/components/Wordle.js
@@ -0,0 +1,25 @@
+import React, {useEffect} from "react";
+import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
+import Keypad from "./Keypad";
+export default function Wordle({solution}) {
+    const { currentGuess, handleKeyUp, guesses, turn, isCorrect} = useWordle(solution)
+
+    useEffect(() => {
+        window.addEventListener('keyup' , handleKeyUp)
+
+        return () => window.removeEventListener('keyup' , handleKeyUp)
+    }, [handleKeyUp])
+
+    useEffect(() => {
+        console.log(guesses,turn,isCorrect)
+    }, [guesses, turn, isCorrect])
+    return (
+        <div>
+            <div> solution - {solution}</div>
+            <div>current guess - {currentGuess}</div>
+            <Grid currentGuess = {currentGuess} guesses ={ guesses} turn = {turn} />
+            <Keypad />
+        </div>
+    )
+}
